0d7df9b9d05ed706fa85cf3cd6f5601e9119b547
Spaguetti7 <ycontigosomosgatos@gmail.com>
2025-12-02 23:58:08 +0100

    Update PauseScene.js

--- Juego/src/scene/PauseScene.js
+++ Juego/src/scene/PauseScene.js
@@ -5,23 +5,28 @@ export class PauseScene extends Phaser.Scene{
     }
     create(data){
         this.add.text(400,160,'Pausado',{
-            fontSize: '64px'
+            fontSize: '64px',
+            color: '#EDA3BB'
         }).setOrigin(0.5);
+
         const resumeBtn = this.add.text(400,320, 'Reanudar',{
-            fontSize:'60px'
+            fontSize:'60px',
+            color: '#EDA3BB'
         }).setOrigin(0.5).setInteractive({useHandCursor:true})
-        .on('pointerover',()=>resumeBtn.setColor('#ff0000'))
-        .on('pointerout',()=>resumeBtn.setColor('#00ff00'))
+        .on('pointerover',()=>resumeBtn.setColor('#F4DFE6'))
+        .on('pointerout',()=>resumeBtn.setColor('#EDA3BB'))
         .on('pointerdown', ()=>{
             this.scene.stop();
             this.scene.resume(data.originalScene);
             this.scene.get(data.originalScene).resume();
         });
+
         const homeBtn = this.add.text(400,520, 'Menú inicial',{
-            fontSize:'60px'
+            fontSize:'60px',
+            color: '#EDA3BB'
         }).setOrigin(0.5).setInteractive({useHandCursor:true})
-        .on('pointerover',()=>homeBtn.setColor('#ff0000'))
-        .on('pointerout',()=>homeBtn.setColor('#00ff00'))
+        .on('pointerover',()=>homeBtn.setColor('#F4DFE6'))
+        .on('pointerout',()=>homeBtn.setColor('#EDA3BB'))
         .on('pointerdown', ()=>{
             this.scene.stop();
             this.scene.stop(data.originalScene);
