c1776bca1756ed4f25755635ace8905173ae56c4
ByNiteProYT <asiermartincaballero900@gmail.com>
2025-12-02 23:14:33 +0100

    Update main.js

--- Juego/src/main.js
+++ Juego/src/main.js
@@ -6,6 +6,10 @@ import { PauseScene } from './scene/PauseScene.js';
 import { ResultsScene } from './scene/ResultsScene.js';
 import { CreditsScene } from './scene/CreditsScene.js';
 import { SettingsScene } from './scene/SettingsScene.js';
+<<<<<<< Updated upstream
+=======
+import { ControlsScene } from './scene/ControlsScene.js';
+>>>>>>> Stashed changes
 
 
 const config = {
@@ -21,7 +25,7 @@ const config = {
         } 
     },
 
-    scene: [MenuScene, GameScene, PauseScene, ResultsScene, CreditsScene, SettingsScene],
+    scene: [MenuScene, GameScene, PauseScene, ResultsScene, CreditsScene, SettingsScene,ControlsScene],
     //backgroundColor: '#360246ff'
     backgroundColor: '#ffffff'
 }
