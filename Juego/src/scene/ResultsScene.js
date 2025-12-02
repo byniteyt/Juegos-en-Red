62fd5a575e74c17f2b07a5e20514879b6cc42944
ByNiteProYT <asiermartincaballero900@gmail.com>
2025-12-03 00:12:31 +0100

    assets de juego a resultado

--- Juego/src/scene/ResultsScene.js
+++ Juego/src/scene/ResultsScene.js
@@ -9,7 +9,26 @@ export class ResultsScene extends Phaser.Scene {
         this.load.image('results', 'Assets/Results/Pantalla_de_Resultado.png');
     }
 
-    create() {
+    create(traspaso) {
         this.add.image(600, 350, 'results');
+<<<<<<< Updated upstream
+=======
+        console.log(traspaso.winText);
+        this.add.text(400,100, `${traspaso.winText}`, {
+            fontSize:'64px',
+            color: '#550430ff'
+        }).setOrigin(0.5);
+        this.add.image(250, 400, traspaso.gato).setOrigin(0.5);
+        
+        const mainMenuBtn = this.add.text(850, 130, 'Volver al menú inicial', {
+            fontSize: '24px',
+            color: '#EDA3BB'
+        }).setOrigin(0.5)
+        .setInteractive({ useHandCursor: true })
+        .on('pointerover', () => mainMenuBtn.setStyle({ fill: '#F4DFE6'}))
+        .on('pointerout', () => mainMenuBtn.setStyle({ fill: '#EDA3BB'}))
+        .on('pointerdown', () => { this.scene.start('MenuScene');
+        });
+>>>>>>> Stashed changes
     }
 }
