7b01d6905c1d9ef88292b42f3f97312d6a74fb2f
pixel-alex <blumgogo@gmail.com>
2025-12-02 20:34:43 +0100

    Rediseño del Logo y los Botones

--- Juego/src/scene/MenuScene.js
+++ Juego/src/scene/MenuScene.js
@@ -6,20 +6,23 @@ export class MenuScene extends Phaser.Scene {
     }
 
     preload(){
-        this.load.image('Logo', 'Assets/MainMenu/Logo.png');
-        this.load.image('Conf', 'Assets/MainMenu/Conf.png');
-        this.load.image('Play', 'Assets/MainMenu/Play.png');
-        this.load.image('Title', 'Assets/MainMenu/Title.png');
-        this.load.image('Decoration', 'Assets/MainMenu/Decoration.png');
+        this.load.image('Logo', 'Assets/MainMenu/Final/Logo.png');
+        this.load.image('Conf_Button', 'Assets/MainMenu/Final/Conf_Button.png');
+        this.load.image('Conf_Shadow', 'Assets/MainMenu/Final/Conf_Shadow.png');
+        this.load.image('Play_Button', 'Assets/MainMenu/Final/Play_Button.png');
+        this.load.image('Play_Shadow', 'Assets/MainMenu/Final/Play_Shadow.png');
+
+        this.load.image('Title', 'Assets/MainMenu/Sketch/Title.png');
+        this.load.image('Decoration', 'Assets/MainMenu/Sketch/Decoration.png');
     }
    
     create() {
 
         /* Logo Animado */
 
-        const logo = this.add.image(600, 200, 'Logo')
+        const logo = this.add.image(600, 175, 'Logo')
         .setOrigin(0.5)
-        .setScale(0.25);
+        .setScale(0.1875);
 
         this.logoTilting = false;
         this.logoTiltDirection = 1;
@@ -64,30 +67,51 @@ export class MenuScene extends Phaser.Scene {
 
         /* Botones Interactivos */
 
-        const confBtn = this.add.image(100, 625, 'Conf')
+        const confButton = this.add.image(100, 625, 'Conf_Button')
+        .setDepth(1)
         .setOrigin(0.5)
-        .setScale(0.25)
+        .setScale(0.35)
         .setInteractive({useHandCursor: true})
         .on('pointerdown', () =>{
             this.scene.start('ControlsScene');
         });
 
-        const playBtn = this.add.image(1100, 625, 'Play')
+        const confShadow = this.add.image(95, 630, 'Conf_Shadow')
+        .setDepth(0)
         .setOrigin(0.5)
-        .setScale(0.25)
+        .setScale(0.35);
+
+        const playButton = this.add.image(1100, 625, 'Play_Button')
+        .setDepth(1)
+        .setOrigin(0.5)
+        .setScale(0.35)
         .setInteractive({useHandCursor: true})
         .on('pointerdown', () =>{
             this.scene.start('GameScene');
         });
 
+        const playShadow = this.add.image(1095, 630, 'Play_Shadow')
+        .setDepth(0)
+        .setOrigin(0.5)
+        .setScale(0.35);
+
         this.tweens.add({
-            targets: [confBtn, playBtn],
-            scale: 0.275,
-            y: '-=10',
+            targets: [confButton, playButton],
+            y: '-=15',
             duration: 1000,
             yoyo: true,
             repeat: -1,
             ease: 'Sine.inOut'
+        });
+
+        this.tweens.add({
+            targets: [confShadow, playShadow],
+            y: '-=15',
+            duration: 1000,
+            yoyo: true,
+            repeat: -1,
+            ease: 'Sine.inOut',
+            delay: 150
         });
     }
 }
