05ad2a512581c2807e01841dc2c2a353c40d7d53
Spaguetti7 <ycontigosomosgatos@gmail.com>
2026-01-17 21:08:12 +0100

    Update LoginScene.js

--- Juego/src/client/scene/LoginScene.js
+++ Juego/src/client/scene/LoginScene.js
@@ -1,8 +1,15 @@
 import Phaser from "phaser";
+
 export class LoginScene extends Phaser.Scene {
+
     constructor() {
         super('LoginScene')
     }
+
+    start() {
+        let text = "";
+    }
+
     update(){
         this.input.keyboard.on('keydown',(event)=>{
             if(event.key){
